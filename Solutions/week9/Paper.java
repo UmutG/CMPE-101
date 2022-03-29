6775864a578bcf10c8fa385e1d499111dc85b1ce
Umut Geyik <umut.geyik@hotmail.com>
2022-03-29 23:15:47 +0300

    Add files via upload

---
+++ Solutions/week9/Paper.java
@@ -0,0 +1,38 @@
+// Umut Geyik - 120200145 - CMPE 101.02
+package week9;
+	// Create a class called Paper and assign two parameters:
+	// Paper size  with integer value, its quality with integer value
+public class Paper {
+	public int size = 0;
+	public int quality = 0;
+	
+	// Paper constructor
+	public Paper(int size, int quality) {
+		this.size = size;
+		this.quality = quality;
+	}
+
+	// Getters and setters for paper's size and quality
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getQuality() {
+		return quality;
+	}
+
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+
+	// toString method to return paper's values
+	@Override
+	public String toString() {
+		return "Paper [size=" + size + ", quality=" + quality + "]";
+	}
+	
+}
---
+++ Solutions/week9/Scroll.java
@@ -0,0 +1,34 @@
+// Umut Geyik - 120200145 - CMPE 101.02
+package week9;
+
+// Created Scroll class and inherited Paper class. Thus, I will assign 
+// scroll's size and quality without initializing default values
+public class Scroll extends Paper {
+	// Method will create preserveTime by paper's quality. 
+	// Every one quality increases preserveTime by 17 years
+	
+	public int preserveTime = 0;
+	// To inherit paper class, we have to use its parameters
+	public Scroll(int size, int quality) {
+		// In order to return every single information, 
+		// We need function super() to return all values
+		super(size, quality);
+		for(int i = 0; i <= quality; i++) {
+			this.preserveTime += 17;
+		}
+	}
+	// Getter and setter for preserveTime
+	public int getPreserveTime() {
+		return preserveTime;
+	}
+	public void setPreserveTime(int preserveTime) {
+		this.preserveTime = preserveTime;
+		
+	}
+	@Override
+	// Created toString method to printout information for every scroll
+	public String toString() {
+		return "Scroll " + super.toString() + " [preserveTime=" + preserveTime + "]";
+	}
+	
+}
